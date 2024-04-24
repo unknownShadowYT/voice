df4ab304bc2f3d23c02e7f29270ebe828ddcd68b
unknownShadowYT <157511689+unknownShadowYT@users.noreply.github.com>
2024-04-24 10:27:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client(); 
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+
+const { joinVoiceChannel } = require('@discordjs/voice');
+client.on('ready', () => {
+
+    setInterval( async () => {
+    client.channels.fetch(process.env.channel) 
+     .then((channel) => { 
+      const VoiceConnection = joinVoiceChannel({
+       channelId: channel.id, 
+       guildId: channel.guild.id, 
+       adapterCreator: channel.guild.voiceAdapterCreator 
+       });
+    }).catch((error) => { return; });
+    }, 1000)
+}); 
+
+client.login(process.env.token);
